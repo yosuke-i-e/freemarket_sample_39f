8b69ff0226a25c46c5d41765f6db35ea570bf65d
yosuke-i-e <yosuke.kamiya@gmail.com>
2019-02-12 16:35:56 +0900

    edit deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,8 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.11.0"
 
+set :branch, 'basic_auth'
+
 set :application, "freemarket_sample_39f"
 set :repo_url, "git@github.com:yosuke-i-e/freemarket_sample_39f.git"
 
@@ -15,6 +17,13 @@ set :ssh_options, auth_methods: ['publickey'],
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
+
+set :default_env, {
+  rbenv_root: "/usr/local/rbenv",
+  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
+  BASIC_AUTH_USER: ENV["BASIC_AUTH_USER"],
+  BASIC_AUTH_PASSWORD: ENV["BASIC_AUTH_PASSWORD"]
+}
 
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
