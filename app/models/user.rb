f0b9a26a15bbff69d998f92b3c4d9c24e8108ff9
yosuke-i-e <yosuke.kamiya@gmail.com>
2019-02-17 16:33:02 +0900

    Revert "set validation"
    This reverts commit d166d640136c9900820d0e96f3b86e528ca702be.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,4 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
-  validates :birth_year, presence: true
-  validates :birth_month, presence: true
-  validates :birth_day, presence: true
 end
