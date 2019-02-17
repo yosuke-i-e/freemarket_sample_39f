ada131103d7a46b0d7699516d18cff0fbf17c41c
yosuke-i-e <yosuke.kamiya@gmail.com>
2019-02-17 09:28:25 +0900

    user model test

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,17 @@
 FactoryGirl.define do
 
   factory :user do
+    nickname              "ニックネーム"
     email                 "kkk@gmail.com"
     password              "00000000"
     password_confirmation "00000000"
+    last_name             "さん"
+    first_name            "ぷる"
+    last_name_kana        "サン"
+    first_name_kana       "プル"
+    birth_year            "2000"
+    birth_month           "1"
+    birth_day             "1"
   end
 
 end
