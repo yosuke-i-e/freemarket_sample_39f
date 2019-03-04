e7ce78f772f0bcca4af5966eb9106461e8c14630
yosuke-i-e <yosuke.kamiya@gmail.com>
2019-03-04 14:07:42 +0900

    edit routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
   resources :items, only: :index
-  resources :users, only: [:show, :destroy] do
+  resources :users, only: [:show, :destroy, :edit, :update] do
     get 'logout', to: 'users#logout'
   end
 end
