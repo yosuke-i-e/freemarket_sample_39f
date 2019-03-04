0425267ad7d660d77b357eeab37264c17eb99337
yosuke-i-e <yosuke.kamiya@gmail.com>
2019-03-04 14:09:06 +0900

    edit users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,14 @@ class UsersController < ApplicationController
   def show
   end
 
+  def edit
+    @user = User.find(current_user.id)
+  end
+
+  def update
+    @user = User.find(current_user.id)
+    @user.update(params.require(:user).permit(:nickname, :profile))
+    redirect_to edit_user_path
+  end
 
 end
