25fb2b3bf6e19002711b79817254b7b233d1b382
yosuke-i-e <yosuke.kamiya@gmail.com>
2019-03-24 10:51:13 +0900

    edit users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,22 @@
 class UsersController < ApplicationController
 
+  before_action :set_user, only: [:edit, :update]
+
   def show
   end
 
   def edit
-    @user = User.find(current_user.id)
   end
 
   def update
-    @user = User.find(current_user.id)
-    @user.update(params.require(:user).permit(:nickname, :profile))
+    @user.update!(params.require(:user).permit(:nickname, :profile))
     redirect_to edit_user_path
+  end
+
+  private
+
+  def set_user
+    @user = User.find(current_user.id)
   end
 
 end
