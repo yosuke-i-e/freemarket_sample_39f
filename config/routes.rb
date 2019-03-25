0b3170cd0e1ba1132f67ec825df90e5dba6300cc
yosuke-i-e <yosuke.kamiya@gmail.com>
2019-03-25 18:22:06 +0900

    edit routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
-  resources :items, only: :index
+  get 'items/index', to: 'items#index'
   resources :users, only: [:show, :destroy] do
+    resources :items, only: [:new, :create] do
+      resources :item_images, only: [:new, :create]
+    end
     get 'logout', to: 'users#logout'
   end
 end
