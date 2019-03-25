efebe71fde2962b772e70df599fcb159f72947ef
yosuke-i-e <yosuke.kamiya@gmail.com>
2019-03-25 18:22:32 +0900

    edit items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,10 +4,30 @@ class ItemsController < ApplicationController
     @items = Item.order("created_at DESC").includes(:item_images).limit(top_items)
   end
 
+  def new
+    @user = User.find(current_user.id)
+    @item = Item.new
+    @item.item_images.build
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.save!
+    if @item.save
+      redirect_to user_path(current_user)
+    else
+      render 'new'
+    end
+  end
+
   private
 
   def top_items
     return 4
+  end
+
+  def item_params
+    params.require(:item).permit(:name, :price, :description, :category_id, :postage, :shipping_date, :state, :region_id, item_images_attributes: [:id, :body]).merge(user_id: current_user.id)
   end
 
 end
