989f1207935743ad2f46a8977259cf6b5b59c984
yosuke-i-e <yosuke.kamiya@gmail.com>
2019-03-25 17:57:26 +0900

    set association to item

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,17 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  belongs_to :category
+  belongs_to :region
   has_many :item_images
+  accepts_nested_attributes_for :item_images
+
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :user_id, presence: true
+  validates :description, presence: true
+  validates :category_id, presence: true
+  validates :postage, presence: true
+  validates :shipping_date, presence: true
+  validates :state, presence: true
+  validates :region_id, presence: true
 end
